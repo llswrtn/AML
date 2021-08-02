0f84960a24aabfe3f9eb73e5a44d188d2846fd3a
Christian Lang <christian.lang92@gmx.de>
2021-08-02 23:52:14 +0200

    improved GetImagePath

--- data_wrapper_images.py
+++ data_wrapper_images.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 class DataWrapperImages:
@@ -6,10 +7,21 @@ class DataWrapperImages:
         self.data_frame = pd.read_csv ("data/train_image_level.csv")
 
     def GetImagePath(self, row_index):
-        image_id = self.data_frame.loc[row_index,"id"]
+        image_id = self.data_frame.loc[row_index,"id"].replace("_image", ".dcm")
         #boxes =  self.data_frame.loc[row_index,"boxes"]
         #label =  self.data_frame.loc[row_index,"label"]
-        series_id = "81456c9c5423" #TODO get correct series id
-        instance_id =  self.data_frame.loc[row_index,"StudyInstanceUID"]
-        path = ("data/train/" + instance_id + "/" + series_id + "/" + image_id).replace("_image", ".dcm")
-        return path
+        instance_id =  self.data_frame.loc[row_index,"StudyInstanceUID"]        
+        path = "data/train/" + instance_id
+        children = [f for f in os.scandir(path) if f.is_dir()]
+        
+        if len(children) == 0:
+            print("error no children")
+        elif len(children) == 1:
+            return (children[0].path + "/" + image_id)
+        else:
+            for i, child in enumerate(children):
+                potential_path = child.path + "/" + image_id
+                if os.path.exists(potential_path):
+                    return (children[0].path + "/" + image_id)
+            print("error no child with image_id")
+        return ""
