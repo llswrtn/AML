2be0353a23f8cb06d33fc8ebf765f4064724633b
Christian Lang <christian.lang92@gmx.de>
2021-08-02 23:24:44 +0200

    plot image 0

--- data_wrapper_images.py
+++ data_wrapper_images.py
@@ -9,6 +9,7 @@ class DataWrapperImages:
         image_id = self.data_frame.loc[row_index,"id"]
         #boxes =  self.data_frame.loc[row_index,"boxes"]
         #label =  self.data_frame.loc[row_index,"label"]
+        series_id = "81456c9c5423" #TODO get correct series id
         instance_id =  self.data_frame.loc[row_index,"StudyInstanceUID"]
-        path = ("data/" + instance_id + "/" + image_id).replace("_image", ".dcm")
+        path = ("data/train/" + instance_id + "/" + series_id + "/" + image_id).replace("_image", ".dcm")
         return path
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from data_wrapper_images import DataWrapperImages
-
+import pydicom as dicom
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     data_wrapper_images = DataWrapperImages()
     path = data_wrapper_images.GetImagePath(0)
     print(path)
-
+    ds = dicom.dcmread(path)
+    plt.imshow(ds.pixel_array)
+    plt.show()
